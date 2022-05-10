653a5cfefb9a2383bacdf48cfeee97d95b509a74
adddd5706 <105033857+adddd5706@users.noreply.github.com>
2022-05-10 22:30:07 +0800

    Update fezskill.js

--- docs/js/fezskill.js
+++ docs/js/fezskill.js
@@ -201,7 +201,7 @@ Skill.prototype = {
 };
 $(document).ready(function() {
 	// ツールチップ設定
-	$("span.skillname").powerTip({placement: "n", smartPlacement: false});
+	$("span.skillname").powerTip({placement: "n", smartPlacement: true});
 
 	// スキルアイコンドラッグ
 	$(".skillicon").draggable({helper: "clone"});
